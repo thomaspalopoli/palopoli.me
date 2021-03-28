936602dc7f856fddaa683e87e1472e2c3501556c
Thomas Palopoli <palopolithomas71@gmail.com>
2021-03-28 13:10:24 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
